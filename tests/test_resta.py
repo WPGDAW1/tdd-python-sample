07f02e4b695a7611ee583d272934a5bf8e101941
Walter Ponce <walpongar@alu.edu.gva.es>
2024-06-10 19:18:17 +0200

    Nueva correción de errores + implementación de funciones

--- tests/test_resta.py
+++ tests/test_resta.py
@@ -10,6 +10,6 @@ class TestClass:
     # Test para la operación suma
     def test_resta(self):
         assert resta(5,5) == 0
-        assert resta(1,2) == -1
-        assert resta(-7,8) == 1
-        assert resta(-7,9) == 2
+        assert resta(2,1) == 1
+        assert resta(7,2) == 5
+        assert resta(9,7) == 2
